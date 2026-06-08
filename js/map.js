967d1c2e9bad281eef335ec1cf510aa85850fcb1
Ahishakiyeyv <yvanahishakiye6@gmail.com>
2026-06-08 21:46:49 +0200

    some layers have been added

--- js/map.js
+++ js/map.js
@@ -1,7 +1,38 @@
+//Initialize the map and set its view to the specified coordinates and zoom level
 var map = L.map("map").setView([-3.3731, 29.9189], 9);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "&copy; OpenStreetMap contributors",
 }).addTo(map);
 
-L.marker([-3.3731, 29.9189]).addTo(map).bindPopup("Burundi").openPopup();
+//Fetching GeoJSON data for Burundi and adding it to the map
+fetch("data/country.geojson")
+  .then((response) => response.json())
+  .then((data) => {
+    L.geoJSON(data, {
+      style: {
+        color: "#7d8b8f",
+        weight: 3,
+        fillOpacity: 0.3,
+      },
+    })
+      .bindPopup((layer) => {
+        return layer.feature.properties.NAME_1;
+      })
+      .addTo(map);
+  })
+  .catch((error) => console.error("Error loading GeoJSON data:", error));
+
+// Fecthing Roads GeoJSON data and adding it to the map
+fetch("data/RNs.geojson")
+  .then((response) => response.json())
+  .then((data) => {
+    L.geoJSON(data, {
+      style: {
+        color: "#ff5733",
+        weight: 1,
+        dashed: true,
+      },
+    }).addTo(map);
+  })
+  .catch((error) => console.error("Error loading Roads GeoJSON data:", error));
